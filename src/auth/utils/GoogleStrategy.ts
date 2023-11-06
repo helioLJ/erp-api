5f6fc282266a0a83c0f57a376d61a321301581bf
HelioLucio <helio.lucio.jr@hotmail.com>
2023-11-06 15:54:53 -0300

    created googlestrategy and set it up with env variables using nestconfig

---
+++ src/auth/utils/GoogleStrategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
+      clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET_KEY'),
+      callbackURL: 'http://localhost:3000/api/auth/google/redirect',
+      scope: ['profile', 'email'],
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: Profile) {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+  }
+}
